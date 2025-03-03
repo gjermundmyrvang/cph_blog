c6c5ec1de657184e791fc2cd610df43e7f3dc70d
gjermundmyrvang <myrvang.gjermund@gmail.com>
2025-03-03 16:40:43 +0100

    add link to map

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -10,6 +10,7 @@ export function Footer() {
             Ha en trivelig dag!
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+          <a href="https://www.google.com/maps/d/edit?mid=1yC5ajUUyJjMZ7rhPbeG6ri_D2Ny0xFA&usp=sharing" rel="noopener noreferrer" target="_blank" className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">Sjekk kartet</a>
           </div>
         </div>
       </Container>
